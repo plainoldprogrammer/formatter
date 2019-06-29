f801c44e6a3b6667034c449d7ed87e8db25454c1
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-06-28 23:27:01 -0500

    configuring the main window to show when the application starts

--- formatter.cpp
+++ formatter.cpp
@@ -1,8 +1,17 @@
 #include "pch.h"
+#include "MainWindow.h"
+
 
 using namespace System;
+using namespace System::Windows::Forms;
 
 int main(array<System::String ^> ^args)
 {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	formatter::MainWindow mainWindow;
+	Application::Run(%mainWindow);
+	
     return 0;
 }
